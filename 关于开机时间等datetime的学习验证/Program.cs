82c4e62ac526da938da38d5c53a3be11799832b2
qq1012803704 <1012803704@qq.com>
2016-01-11 03:24:42 +0800

    时间的学习

---
+++ 关于开机时间等datetime的学习验证/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 关于开机时间等datetime的学习验证
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = DateTime.Now.AddMilliseconds(0 - Environment.TickCount);
+
+            DateTime dt2 = DateTime.Now;
+
+            
+            Console.WriteLine("变量dt="+dt.ToString());
+
+            Console.WriteLine("变量dt2=" + dt2.ToString());
+
+            Console.WriteLine("变量dt3=" + (dt2 - dt));
+
+            Console.WriteLine("Environment.TickCount=" + Environment.TickCount);
+            
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 方法和类的使用/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 方法和类的使用
+{
+    class Program
+    {
+        class ClassMax
+        {
+            public int IntMax(int a,int b)
+            {
+                return a > b ? a : b;
+            }
+        }
+        /// <summary>
+        /// 类》声明对象》访问对象
+        /// 类的定义
+        /// class 类名
+        /// {
+        /// 
+        /// }
+        /// 例如声明一个叫student的类：
+        /// class student
+        /// {
+        ///     public string name;
+        /// }
+        /// 
+        /// 声明对象
+        /// 类名 对象名;
+        /// 例如声明一个叫s1的对象：
+        /// student s1;
+        /// 
+        /// 访问对象
+        /// 对象名.类的字段
+        /// 例如访问s1的name字段：
+        /// s1.name
+        /// 
+        /// 
+        /// 声明方法
+        /// 访问修饰符 返回类型 方法名（参数列表）
+        /// {
+        /// 
+        /// }
+        /// 例如比较两个数的大小的一个方法：
+        /// public int IntMax（int a，int b）
+        /// {
+        ///     一段比较的语句块
+        /// }
+        /// 在声明的类中调用方法
+        /// 方法名（参数列表）
+        /// return IntMax（a，b）
+        /// 
+        /// 在声明的类外调用方法
+        /// 对象名.方法名（参数列表）
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        ///         
+
+        static void Main(string[] args)
+        {
+            
+            int x = 41, y;
+            y = 100;
+            ClassMax a = new ClassMax();
+            int c = a.IntMax(x, y);
+            Console.WriteLine(c);
+            Console.ReadKey();
+        }
+       
+    }
+}
